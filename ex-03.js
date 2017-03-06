12c8e84f6abba037502a930ba26bbc6b756595f9
travis <t3patterson@gmail.com>
2017-03-06 08:52:22 -0500

    cleanup

--- ex-03.js
+++ ex-03.js
@@ -12,7 +12,7 @@ function calcMinutes(){
 
 
 let commuteToWorkTimes = ["4:18", "1:11:22", "21:29", "11:41", "8:56"]
-console.assert(calcMinutes(walkAroundCityTimes) === 230)
+console.assert(calcMinutes(commuteToWorkTimes) === 230)
 
 let walkAroundCityTimes = ["04:30:10", "1:03:44", "06:08:21", "12:10:21", "24:33:18"]
 console.assert(calcMinutes(walkAroundCityTimes) === 2906)
